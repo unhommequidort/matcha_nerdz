838dd32d8362226f1736c577114227054c61287c
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-01-26 14:37:16 -0600

    Activates LiveReload

--- config.rb
+++ config.rb
@@ -48,7 +48,7 @@ end
 page "/feed.xml", layout: false
 # Reload the browser automatically whenever files change
 # configure :development do
-#   activate :livereload
+  activate :livereload
 # end
 
 # Methods defined in the helpers block are available in templates
